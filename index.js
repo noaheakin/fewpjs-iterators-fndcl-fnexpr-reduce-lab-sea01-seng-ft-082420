1a37a55443af21d09946526870205bc474206ae1
noaheakin <noaheakin@gmail.com>
2021-01-18 16:54:17 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-function totalBatteries (batteryBatches) {
-    finalTally = 0
-    batteryBatches.reduce(e => finalTally += e)
-    return finalTally
-}
+let totalBatteries = batteryBatches.reduce((acc, batch) => acc + batch, 0)
